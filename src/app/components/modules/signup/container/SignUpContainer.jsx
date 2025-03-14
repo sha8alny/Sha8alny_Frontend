50861d280ca2d82e451d2a1515c0e752d9771852
Salsabil <salsabilmostafa2003@gmail.com>
2025-03-14 15:24:05 -0700

    added documentation comments

--- src/app/components/modules/signup/container/SignUpContainer.jsx
+++ src/app/components/modules/signup/container/SignUpContainer.jsx
@@ -1,11 +1,20 @@
 "use client";
-import { useState, useEffect } from "react";
 import { useMutation } from "@tanstack/react-query";
 import { useRouter } from "next/navigation";
 import SignUpForm from "../presentation/SignUpForm";
 import { handleSignup } from "../../../../services/userMangment";
 
-
+/**
+ * SignUpContainer component handles the user registration process.
+ * It uses a mutation to call the signup service and handles the success and error responses.
+ * On successful registration, it redirects the user to the signin page.
+ * 
+ * @component
+ * @example
+ * return (
+ *   <SignUpContainer />
+ * )
+ */
 const SignUpContainer = () => {
     const router = useRouter();
     
@@ -20,6 +29,13 @@ const SignUpContainer = () => {
         }
     });
 
+    /**
+     * Handles the form submission.
+     * Checks if the reCAPTCHA is verified before proceeding with the signup mutation.
+     * 
+     * @param {Object} formData - The data submitted from the signup form.
+     * @param {boolean} formData.recaptcha - The reCAPTCHA verification status.
+     */
     const handleSubmit = (formData) => {
         if(!formData.recaptcha){
             alert("Please verify that you are not a robot");
