8f579d921f9918f0185ff4cac5b277fd88d655d2
EzzCode <ezz05042003@gmail.com>
2025-03-15 01:02:01 +0200

    Revert "feat: update layout to include theme and toast providers, and modify metadata"
    This reverts commit 6054e29f5c9c40e60485752590dee71b9710415c.

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,10 +1,18 @@
 import "./globals.css";
 import { ThemeProvider } from "./context/ThemeContext";
+<<<<<<< HEAD
 import { ToastProvider } from "./context/ToastContext";
 import ReactQueryProvider from "./providers/ReactQueryProvider";
 export const metadata = {
   title: "Shaغlny",
   description: "i need job",
+=======
+import ReactQueryProvider from "./providers/ReactQueryProvider";
+
+export const metadata = {
+  title: "Shaغlny",
+  description: "I need job",
+>>>>>>> parent of a31f319 (Merge remote-tracking branch 'origin/development' into jobs-page)
 };
 
 export default function RootLayout({ children }) {
@@ -12,9 +20,13 @@ export default function RootLayout({ children }) {
     <html lang="en">
       <body>
         <ReactQueryProvider>
+<<<<<<< HEAD
           <ThemeProvider>
             <ToastProvider>{children}</ToastProvider>
           </ThemeProvider>
+=======
+          <ThemeProvider>{children}</ThemeProvider>
+>>>>>>> parent of a31f319 (Merge remote-tracking branch 'origin/development' into jobs-page)
         </ReactQueryProvider>
       </body>
     </html>
--- src/app/services/userProfile.js
+++
@@ -1,8 +0,0 @@
-export const fetchUserProfile = async (username) => {
-    const response = await fetch(`http://localhost:5000/profile/${username}`);
-    if (!response.ok) {
-      throw new Error("Failed to fetch user profile");
-    }    
-    return response.json();
-  };
-  
