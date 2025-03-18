4eae7b98956ba67e26943fb70bd201aef92b743e
Hussein Essam <112550601+Husseinn-Essam@users.noreply.github.com>
2025-03-18 02:59:31 +0200

    feat: create MembershipPage component to render MembershipPageContainer

---
+++ src/app/membership-page/page.js
@@ -0,0 +1,7 @@
+"use client";
+import MembershipPageContainer from "@/app/components/modules/membership/container/MembershipPageContainer";
+const Page = () => {
+  return <MembershipPageContainer />;
+};
+
+export default Page;
