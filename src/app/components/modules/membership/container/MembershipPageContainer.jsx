f7d6c9d3af52d7d616cd2c5006ed9fc50fa4e147
Hussein Essam <112550601+Husseinn-Essam@users.noreply.github.com>
2025-03-18 12:57:43 +0200

    feat: integrate plans details fetching and display in MembershipPage component

--- src/app/components/modules/membership/container/MembershipPageContainer.jsx
+++ src/app/components/modules/membership/container/MembershipPageContainer.jsx
@@ -1,15 +1,19 @@
 "use client";
 
 import { useQuery, useMutation, useQueryClient } from "@tanstack/react-query";
-import { fetchSubscription, cancelSubscription } from "@/app/services/payment";
+import {
+  fetchSubscription,
+  cancelSubscription,
+  fetchPlansDetails,
+} from "@/app/services/payment";
 import MembershipPage from "../presentation/MembershipPage";
 import { useToast } from "@/app/context/ToastContext";
-import {  useRouter } from "next/navigation";
+import { useRouter } from "next/navigation";
 
 /**
  * MembershipPageContainer component manages the state and logic for the membership page.
  * It handles fetching the subscription status, cancelling the subscription, and navigating to the upgrade page.
- * 
+ *
  * @component
  * @returns {JSX.Element} The rendered MembershipPage component with the current subscription details and actions.
  */
@@ -21,6 +25,14 @@ const MembershipPageContainer = () => {
   const { data, isLoading, error } = useQuery({
     queryKey: ["membershipStatus"],
     queryFn: fetchSubscription,
+  });
+  const {
+    data: plansDetails,
+    isLoading: isPlansLoading,
+    error: plansError,
+  } = useQuery({
+    queryKey: ["plansDetails"],
+    queryFn: fetchPlansDetails,
   });
 
   const cancelMutation = useMutation({
@@ -43,17 +55,35 @@ const MembershipPageContainer = () => {
     router.push("/membership-page/payment");
   };
 
-  if (isLoading) return <p className="text-center text-lg">Loading...</p>;
-  if (error)
+  if (isLoading || isPlansLoading)
+    return <p className="text-center text-lg">Loading...</p>;
+  if (error || plansError)
     return (
-      <p className="text-center text-lg text-red-500">Error: {error.message}</p>
+      <p className="text-center text-lg text-red-500">
+        Error: {error?.message || plansError?.message}
+      </p>
     );
 
+  const { max_connections, job_applications, daily_messages } = data?.limits;
+  const limits = {
+    daily_job_applications: job_applications,
+    daily_message_requests: daily_messages,
+    monthly_connection_requests: max_connections,
+  };
 
+  const plans_details = plansDetails;
+
+  const premiumPlanDetails = plans_details.find(
+    (plan) => plan.plan_id === "premium"
+  );
+
+  const freePlanDetails = plans_details.find((plan) => plan.plan_id === "free");
   return (
     <MembershipPage
       currentPlan={data?.status}
-      limits={data?.limits}
+      limits={limits}
+      premiumPlanDetails={premiumPlanDetails}
+      freePlanDetails={freePlanDetails}
       renewalDate={data?.renewal_date}
       isMissed={data?.is_missed}
       handleCancelSubscription={handleCancelSubscription}
