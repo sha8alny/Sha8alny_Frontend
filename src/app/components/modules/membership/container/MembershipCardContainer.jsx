276f5abf715d891adeead2ce168167a7e832a762
Hussein Essam <112550601+Husseinn-Essam@users.noreply.github.com>
2025-03-18 02:41:52 +0200

    doc:  added documentation in MembershipCardContainer component

--- src/app/components/modules/membership/container/MembershipCardContainer.jsx
+++ src/app/components/modules/membership/container/MembershipCardContainer.jsx
@@ -3,15 +3,17 @@ import MembershipCardPresentation from "../presentation/MembershipCardPresentati
 
 /**
  * MembershipCardContainer component manages the state and logic for the membership card.
- *
+ * @component
  * @param {Object} props - The component props.
  * @param {string} props.plan - The plan name.
  * @param {string} props.price - The plan price.
  * @param {Array} props.items - The list of plan features.
- * @param {function} props.handleUpgrade - Function to handle subscription.
  * @param {string} props.currentPlan - The current plan of the user.
  * @param {string} props.renewalDate - The renewal date of the plan.
- * @param {boolean} props.isMissed - Indicates if the plan is missed.
+ * @param {boolean} props.isMissed - Indicates if the renewal date is missed.
+ * @param {function} props.handleUpgrade - Function to handle subscription upgrade.
+ * @param {function} props.handleCancel - Function to handle subscription cancellation.
+ * @param {boolean} props.isCancelling - Indicates if the cancellation process is ongoing.
  * @returns {JSX.Element} The rendered component.
  */
 const MembershipCardContainer = ({
