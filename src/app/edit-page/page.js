50a25074bab0f68ad2c751826e30e6851309cf0b
nadabdallah <nadabdallah25@gmail.com>
2025-03-21 20:39:39 +0200

    edit-page

---
+++ src/app/edit-page/page.js
@@ -0,0 +1,12 @@
+"use client";
+import EditContainer from "../components/modules/company-page-author/container/EditContainer";
+
+function EditCompanyPage(){
+    return(
+        <div>
+            <EditContainer/> 
+        </div>
+    );
+}
+
+export default EditCompanyPage;
