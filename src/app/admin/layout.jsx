14a2ca0fdce637427311d7868927da77650c8c82
Hussein Essam <112550601+Husseinn-Essam@users.noreply.github.com>
2025-03-21 11:02:41 +0200

    feat: add flagged job listings page for admin

---
+++ src/app/admin/layout.jsx
@@ -0,0 +1,10 @@
+import { SidebarContainer } from "../components/modules/admin/container/SidebarContainer";
+
+export default function AdminLayout({ children }) {
+  return (
+    <div className="flex h-screen  font-sans bg-background text-text">
+      <SidebarContainer />
+      <main className="flex-1 overflow-auto p-4 md:p-6">{children}</main>
+    </div>
+  );
+}
