faa975abf14f8de895d4b8caa30e8015425737bd
ZiadHF <ziadhfahmy@yahoo.com>
2025-03-21 16:56:25 +0200

    user profile page

---
+++ src/app/components/modules/profile/presentation/ProfilePresentation.jsx
@@ -0,0 +1,46 @@
+import Footer from "@/app/components/layout/Footer";
+import SuggestedUsersContainer from "../container/SuggestedUsersContainer";
+import About from "./About";
+import ProfileStrength from "./ProfileStrength";
+import ProfileHeader from "./ProfileHeader";
+import Skills from "./Skills";
+import ChangeURL from "./ChangeURL";
+import ExperienceContainer from "../container/ExperienceContainer";
+import EducationContainer from "../container/EducationContainer";
+import CertificationsContainer from "../container/CertificationsContainer";
+
+export function ProfileSkeleton() {}
+
+export default function ProfilePresentation({
+  userProfile,
+  profileStrength,
+  isMyProfile,
+}) {
+  return (
+    <div>
+      <main className="flex gap-2 p-8 bg-foreground">
+        <section className="flex-[3_1_0]">
+          <ProfileHeader userProfile={userProfile} />
+          <About about={userProfile.about} />
+          <ExperienceContainer experience={userProfile.experience} />
+          <EducationContainer education={userProfile.education} />
+          <Skills skills={userProfile.skills} />
+          <CertificationsContainer
+            certifications={userProfile.certifications}
+          />
+        </section>
+        <section className="flex-1 rounded-3xl space-y-2">
+          {isMyProfile && (
+            <>
+              <ChangeURL username={userProfile.username} />
+              <ProfileStrength profileStrength={profileStrength} />
+            </>
+          )}
+          <SuggestedUsersContainer title="People Also Viewed" />
+          <SuggestedUsersContainer title="People You May Know" />
+          <Footer />
+        </section>
+      </main>
+    </div>
+  );
+}
---
+++ src/app/u/[username]/page.js
@@ -0,0 +1,44 @@
+import Navbar from "@/app/components/layout/Navbar";
+import { ProfileContainer } from "@/app/components/modules/profile/container/ProfileContainer";
+import ReactQueryProvider from "@/app/providers/ReactQueryProvider";
+
+
+export async function generateMetadata({ params }) {
+  const { username } = await params;
+  
+  if (!username) {
+    return {
+      title: 'Profile Page',
+      description: 'This is a profile page.',
+    };
+  }
+
+  return {
+    title: `Shaغalny - ${username}`,
+    description: `This is the profile page for ${username}.`,
+    openGraph: {
+      title: `Shaغalny - ${username}`,
+      description: `This is the profile page for ${username}.`,
+      images: [
+        {
+          url: `https://lorempicsum.com/200`,
+          width: 1200,
+          height: 630,
+          alt: `Profile image for ${username}`,
+        },
+      ],
+    },
+  };
+}
+
+export default async function Page({ params }) {
+  const { username } = await params;
+  return (
+    <div className="w-screen h-screen overflow-auto">
+      <Navbar />
+      <ReactQueryProvider>
+        <ProfileContainer username={username}/>
+      </ReactQueryProvider>
+    </div>
+  );
+}
