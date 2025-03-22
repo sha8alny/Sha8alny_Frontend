4888ae217598b9049262824d8ca98afec35be51b
Salsabil <salsabilmostafa2003@gmail.com>
2025-03-22 11:43:17 -0700

    Refactor JobsForm and ProfileCard components for improved layout and styling

--- src/app/components/ui/ProfileCard.jsx
+++ src/app/components/ui/ProfileCard.jsx
@@ -10,35 +10,35 @@ const ProfileCard = ({
     showButton
 }) => {
 return(
-    <div className="max-w-xl sm:max-w-2xl md:max-w-lg bg-foreground shadow-lg rounded-lg overflow-hidden border border-secondary">
+    <div className="max-w-sm sm:max-w-md md:max-w-lg lg:max-w-xl bg-foreground shadow-lg rounded-lg overflow-hidden border border-secondary">
     <div className="h-30">
         {/* // cover image */}
-        <div className="relative h-32 sm:h-40 md:h-48">
+        <div className="relative w-full h-32 sm:h-40 md:h-48">
         <img
         src={coverPic}
         alt="cover"
         className="w-full h-full object-cover"
         />
         {/* // remove button */}
-        <button className="absolute top-2 right-2 bg-None hover:bg-background text-secondary border-1 border-secondary p-1 h-8 w-8 rounded-2xl  mt-2">✖</button>
+        <button className="absolute top-2 right-2 bg-transparent hover:bg-background text-secondary border-1 border-secondary p-1 h-8 w-8 rounded-full  mt-2">✖</button>
     </div>
     </div>
 
-    <div className="p-4 text-center flex flex-col flex-wrap justify-center border-secondary border-t">
-        <div className="flex justify-center -mt-16 sm:-mt-20"> 
+    <div className="p-4 text-center flex flex-col items-center border-secondary border-t">
+        <div className="relative -mt-16 sm:-mt-20 w-24 h-24 sm:w-36 sm:h-36"> 
             {/* // profile image */}
             <img 
             src={profilePic}
             alt="profile"
-            className="w-35 h-35 sm:h-37 sm:w-37 rounded-full border-2 border-secondary "
+            className="w-full h-full rounded-full border-2 border-secondary object-cover "
             />
         </div>
-        <h2 className="text-xl sm:text-2xl font-semibold text-text ">{name}</h2>
-        <p className="text-text text-sm sm:text-base font-thin break-words whitespace-normal w-full">{title}</p>
+        <h2 className="text-lg sm:text-2xl font-semibold text-text mt-2">{name}</h2>
+        <p className="text-sm sm:text-base font-thin text-text break-words whitespace-normal max-w-xs sm:max-w-sm ">{title}</p>
         {showButton && (
         <button 
         onClick={buttonAction}
-        className="bg-None hover:bg-background text-secondary border-1 border-secondary p-2 rounded-lg mt-2">{buttonText}</button>
+        className="bg-transparent hover:bg-background text-secondary border-1 border-secondary p-2 rounded-lg mt-2 transition duration-300">{buttonText}</button>
         )}
     </div>         
     </div> 
