fbc42a7b5204c135a382aa0f1aeaff0f00ca534f
Hussein Essam <112550601+Husseinn-Essam@users.noreply.github.com>
2025-03-23 20:07:26 +0200

    feat: add manage jobs page to admin dashboard and pagination

--- src/app/services/jobs.js
+++ src/app/services/jobs.js
@@ -35,13 +35,11 @@ export const fetchJobDetails = async (id) => {
 };
 
 export const fetchJobListingsPageNumber = async ({ pageParam = 1 }) => {
-  const itemsPerPage = 5;
+  const itemsPerPage = 10;
   const url = new URL(
     `${API_URL}/jobs/search/${pageParam}`
   );
-  url.searchParams.append("page", pageParam); 
-  url.searchParams.append("limit", itemsPerPage);
-  // console.log(url.toString()); 
+
 
   const response = await fetch(url.toString());
   if (!response.ok) {
