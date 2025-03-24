193e9b6845bf70a69bb039f04057bdb40e292d46
nadabdallah <nadabdallah25@gmail.com>
2025-03-24 17:15:11 +0200

    UpdateCheckBoxField

--- src/app/components/modules/company-page-form/presentation/CheckBoxField.jsx
+++ src/app/components/modules/company-page-form/presentation/CheckBoxField.jsx
@@ -1,13 +1,11 @@
-import RemoveCircleOutlineIcon from '@mui/icons-material/RemoveCircleOutline';
-function checkBoxField({onChange, checked, showerror}) {
+function checkBoxField({onChange, checked}) {
   return (
     <div>
-        <div className="flex items-start gap-2">
-            <input className="w-5 h-5 accent-blue-500" type="checkbox" id="company-terms"  checked={checked} onChange={onChange} />
+        <div className="flex text-text items-start gap-2">
+            <input className="w-5 h-5 accent-blue-500" type="checkbox" id="company-terms" checked={checked} onChange={onChange} />
             <label htmlFor="company-terms">I verify that I am an authorized representative of this organization and have the right to act on its behalf in the creation and management of this page. The organization and I agree to the additional terms for Pages.</label>
             <br/>
         </div>
-        {showerror && (<p className="flex items-center gap-2 text-red-500 text-sm"><RemoveCircleOutlineIcon className="w-4 h-4"/>Please select the terms and conditions.</p>)}
     </div>
   );
 }
