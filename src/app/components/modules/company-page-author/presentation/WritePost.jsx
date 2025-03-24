288b7c21f3bff94abfa3944561bcad9c9f51d1a0
nadabdallah <nadabdallah25@gmail.com>
2025-03-24 17:47:53 +0200

    WritePostPresentationUpdate

--- src/app/components/modules/company-page-author/presentation/WritePost.jsx
+++ src/app/components/modules/company-page-author/presentation/WritePost.jsx
@@ -1,12 +1,11 @@
 "use client";
 import ImageOutlinedIcon from '@mui/icons-material/ImageOutlined';
 import SmartDisplayOutlinedIcon from '@mui/icons-material/SmartDisplayOutlined';
-import CalendarMonthOutlinedIcon from '@mui/icons-material/CalendarMonthOutlined';
 import FeedOutlinedIcon from '@mui/icons-material/FeedOutlined';
 
-function WritePost({text, setText,onChange, preview, triggerFileInput,fileInputRef,onSubmit, logoPreview}) {
+function WritePost({text, setText,onImageUpload, onVideoUpload, preview, triggerFileInput,imageInputRef,videoInputRef,onSubmit, logoPreview, openArticleModal}) {
   return (
-    <div>
+    <div className="text-text">
         <div className="w-145 mt-5 border border-[var(--secondary)] rounded-lg p-4">
             <div className="flex items-center gap-4">
                 {logoPreview ? (
@@ -24,17 +23,20 @@ function WritePost({text, setText,onChange, preview, triggerFileInput,fileInputR
             {/*Icons for writing a post*/}
             <div className="flex items-center gap-4 mt-4 justify-between">
                 <div htmlFor="upload-file" className="relative group">
-                    <div className="relative group" onClick={triggerFileInput}>
+                    <div className="relative group" onClick={()=>triggerFileInput("image")}>
                         <ImageOutlinedIcon className="cursor-pointer group"/>
                         <span className="absolute top-full left-1/2 -translate-x-1/2 whitespace-nowrap opacity-0 group-hover:opacity-100 text-white text-xs transition-opacity duration-200 "> Upload Image</span>
                     </div>
-                    <input id="upload-file" type="file" className="hidden" accept="image/png, image/jpg, image/jpeg" ref={fileInputRef} onChange={onChange}/>
+                    <input id="upload-file" type="file" className="hidden" accept="image/png, image/jpg, image/jpeg" ref={imageInputRef} onChange={onImageUpload}/>
                 </div>
-                <div className="relative group">
-                    <SmartDisplayOutlinedIcon className="cursor-pointer group"/>
-                    <span className="absolute top-full left-1/2 -translate-x-1/2 whitespace-nowrap opacity-0 group-hover:opacity-100 text-white text-xs transition-opacity duration-200 "> Upload video</span>
+                <div htmlFor="upload-video" className="relative group">
+                    <div className="relative group" onClick={()=>triggerFileInput("video")}>
+                        <SmartDisplayOutlinedIcon className="cursor-pointer group"/>
+                        <span className="absolute top-full left-1/2 -translate-x-1/2 whitespace-nowrap opacity-0 group-hover:opacity-100 text-white text-xs transition-opacity duration-200 "> Upload video</span>
+                    </div>
+                    <input id="upload-video" type="file" className="hidden" accept="video/mp4, video/mov, video/avi" ref={videoInputRef} onChange={onVideoUpload}/>
                 </div>
-                <div className="relative group">
+                <div className="relative group" onClick={openArticleModal}>
                     <FeedOutlinedIcon className="cursor-pointer group"/>
                     <span className="absolute top-full left-1/2 -translate-x-1/2 whitespace-nowrap opacity-0 group-hover:opacity-100 text-white text-xs transition-opacity duration-200 "> Write Article</span>
                 </div>
