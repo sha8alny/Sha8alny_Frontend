dfc12c17d44f85d8ed53d703f5b2a9870aec35a7
nadabdallah <nadabdallah25@gmail.com>
2025-03-24 15:07:14 +0200

    update-tagline-presentation

--- src/app/components/modules/company-page-form/presentation/TagLine.jsx
+++ src/app/components/modules/company-page-form/presentation/TagLine.jsx
@@ -2,10 +2,10 @@
 function TagLine({companyTagline, setCompanyTagline}){
     return (
       <div className="flex flex-col">
-          <label className="mb-2"> 
+          <label htmlFor="company-tagline" className="mb-2 text-text"> 
             Tagline 
           </label>
-          <textarea className="border px-[8px] py-[6px] rounded-[4px]" name="company-tagline" placeholder="ex:An information services firm helping small businesses succeed" maxLength={120} value={companyTagline || ""} onChange={(e)=> setCompanyTagline(e.target.value)} />
+          <textarea className="text-text border px-[8px] py-[6px] rounded-[4px]" id="company-tagline" name="company-tagline" placeholder="ex:An information services firm helping small businesses succeed" maxLength={120} value={companyTagline || ""} onChange={(e)=> setCompanyTagline(e.target.value)} />
           <div className="flex justify-between text-gray-500 text-sm mt-1 gap-1">
             <span className="break-words"> Use your tagline to briefly describe what your organization does. This can be changed later </span>
             <span>{companyTagline?.length??0}/120</span>
