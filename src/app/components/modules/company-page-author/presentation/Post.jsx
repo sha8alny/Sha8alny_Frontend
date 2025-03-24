2ffb2079834d24090c2d921d56cd17ec81318150
nadabdallah <nadabdallah25@gmail.com>
2025-03-24 17:48:41 +0200

    PostPresentationUpdate

--- src/app/components/modules/company-page-author/presentation/Post.jsx
+++ src/app/components/modules/company-page-author/presentation/Post.jsx
@@ -6,9 +6,9 @@ import FavoriteIcon from '@mui/icons-material/Favorite';
 import RepeatIcon from '@mui/icons-material/Repeat';
 import ImageOutlinedIcon from '@mui/icons-material/ImageOutlined';
 
-function Post({username, followers, text, imageUrl, likes, liked, comments=[], onLike, onComment, logoPreview}){
+function Post({username, followers, text, imageUrl,videoUrl, likes, liked, comments=[], onLike, onComment, logoPreview}){
     return(
-        <div className="w-145 mt-2">
+        <div className="text-text w-145 mt-2">
             {/*Post Container*/}
             <div className="mt-4 border border-[var(--secondary)] rounded-lg p-4">
                 <div className="flex items-center gap-4 ">
@@ -23,15 +23,21 @@ function Post({username, followers, text, imageUrl, likes, liked, comments=[], o
                         )
                     }
                     <div className="flex flex-col ">
-                        <p>{username|| "Company Name"}</p>
+                        <p>{username.charAt(0).toUpperCase()+username.slice(1)|| "Company Name"}</p>
                         <p className="text-xs text-gray-300"> {followers} followers</p>
                     </div>
                 </div>
                 <div className="mt-2 p-2">
                     <p className="mt-2 text-white">{text}</p>
-                    {imageUrl && imageUrl.trim() !== "" && (
-                        <img src={imageUrl} alt="Post" className="w-full h-full rounded-lg mt-2" />
-                    )}
+                    {imageUrl && imageUrl.trim() !== null? (
+                        <img src={imageUrl} alt="" className="w-full h-full rounded-lg mt-2" />
+                    ) : null}
+                    {videoUrl && videoUrl.trim() !== "" ? (
+                        <video controls className="w-full rounded-lg mt-2">
+                            <source src={videoUrl} type="video/mp4" />
+                            <source src={videoUrl} type="video/webm" />
+                        </video>
+                    ) : null}
                 </div>
                 <div className="flex flex-row justify-between items-center mt-2 border-t border-[var(--secondary)] pt-2 ">
                     <button className="relative group" onClick={onLike}>
