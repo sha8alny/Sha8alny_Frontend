093a8e5c7ab885e1c8a2749fcfcb134e91113d92
nadabdallah <nadabdallah25@gmail.com>
2025-03-24 17:49:12 +0200

    EditPagePresentationUpdate

--- src/app/components/modules/company-page-author/presentation/EditPage.jsx
+++ src/app/components/modules/company-page-author/presentation/EditPage.jsx
@@ -1,15 +1,15 @@
 "use client";
 
-import InputFieldContainer from "../../company-page-form/container/InputFieldContainer";
-import SelectFieldContainer from "../../company-page-form/container/SelectFieldContainer";
+import EditInputFieldContainer from "../container/EditInputFieldContainer";
+import EditSelectFieldContainer from "../container/EditSelectFieldContainer";
 import TagLineContainer from "../../company-page-form/container/TagLineContainer";
 
 function EditPage({companyName, setCompanyName, companyIndustry, setCompanyIndustry, companyTagline, setCompanyTagline, companySize, setCompanySize, companyType, setCompanyType, companyLocation, setCompanyLocation,companyURL, setCompanyURL, companyWebsite, setCompanyWebsite, onSave, onDiscard, loading, errors, setErrors}){
     return(
         <div className="flex flex-row">
             <div className="w-full p-4 pr-6 space-y-2 border border-[var(--secondary)] mt-5 rounded-lg mx-4">
-                <InputFieldContainer companyName={companyName} setCompanyName={setCompanyName} companyIndustry={companyIndustry} setCompanyIndustry={setCompanyIndustry} companyLocation={companyLocation} setCompanyLocation={setCompanyLocation} companyURL={companyURL} setCompanyURL={setCompanyURL} companyWebsite={companyWebsite} setCompanyWebsite={setCompanyWebsite} errors={errors} setErrors={setErrors}/>
-                <SelectFieldContainer companySize={companySize} setCompanySize={setCompanySize} companyType={companyType} setCompanyType={setCompanyType} errors={errors}/>
+                <EditInputFieldContainer companyName={companyName} setCompanyName={setCompanyName} companyIndustry={companyIndustry} setCompanyIndustry={setCompanyIndustry} companyLocation={companyLocation} setCompanyLocation={setCompanyLocation} companyURL={companyURL} setCompanyURL={setCompanyURL} companyWebsite={companyWebsite} setCompanyWebsite={setCompanyWebsite} errorss={errors} setErrors={setErrors}/>
+                <EditSelectFieldContainer companySize={companySize} setCompanySize={setCompanySize} companyType={companyType} setCompanyType={setCompanyType}/>
                 <TagLineContainer companyTagline={companyTagline} setCompanyTagline={setCompanyTagline}/>
                 <div className="flex flex-row gap-4">
                     <button className="bg-[var(--secondary)] text-[var(--background)] rounded-full cursor-pointer py-2 px-3  mt-2" onClick={onDiscard} disabled={loading}> Discard edits</button>  
