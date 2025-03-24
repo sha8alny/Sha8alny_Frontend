2b3d903625fe59cdd69518649f204f0a1fc9ea7d
nadabdallah <nadabdallah25@gmail.com>
2025-03-24 15:08:03 +0200

    update-selectfield-presentation

--- src/app/components/modules/company-page-form/presentation/SelectField.jsx
+++ src/app/components/modules/company-page-form/presentation/SelectField.jsx
@@ -2,10 +2,10 @@ import EmergencyRoundedIcon from '@mui/icons-material/EmergencyRounded';
 function SelectField({label, name, options, required, optionlabel, selectedvalue, onChange}) {
     return (
         <div className="flex flex-col mb-1">
-            <label className="flex items-center mb-2">
+            <label htmlFor={name} className="flex text-text items-center mb-2">
             {label} {required && <EmergencyRoundedIcon style={{fontSize:"10px"}}/>}
             </label>
-            <select className="border px-[8px] py-[6px] rounded-[4px]" name={name} value={selectedvalue} onChange={(e) => onChange(e.target.value)}>
+            <select className="border text-text px-[8px] py-[6px] rounded-[4px]" id={name} name={name} value={selectedvalue} onChange={(e) => onChange(e.target.value)}>
               <option className="text-black" value="">Select {optionlabel}</option>
               {options.map((option) => (
                   <option className="text-black" key={option.value} value={option.value}>
@@ -16,5 +16,4 @@ function SelectField({label, name, options, required, optionlabel, selectedvalue
         </div>
     )
 }
-
 export default SelectField;
