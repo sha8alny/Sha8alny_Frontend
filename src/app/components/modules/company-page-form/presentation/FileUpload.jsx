dae9119d4321c2882bab5aa381c0ab525d183d66
nadabdallah <nadabdallah25@gmail.com>
2025-03-24 23:40:58 +0200

    file-upload-presentation-update

--- src/app/components/modules/company-page-form/presentation/FileUpload.jsx
+++ src/app/components/modules/company-page-form/presentation/FileUpload.jsx
@@ -4,23 +4,24 @@ import CloseIcon from '@mui/icons-material/Close';
 function FileUpload({onChange, preview, file, onRemove}){
     return(
         <div>
-            <label className="mb-2">
+            <label className="text-text mb-2">
               Logo 
             </label>
             {preview ? 
-            (<div className="flex items-center w-full border border-[(var--foreground)] rounded-lg mt-2 gap-2">
+            (<div className="text-text flex items-center w-full border border-[(var--foreground)] rounded-lg mt-2 gap-2">
                 <img src={preview} alt="Preview" className="w-20 h-15 rounded-tl-lg rounded-bl-lg object-cover"/>
                 <div className="flex flex-col text-white text-sm">
                     <span>{file.name}</span>
                 </div>
-                <button className="ml-auto mr-2 cursor-pointer" onClick={onRemove}><CloseIcon fontSize='small'/></button>
+                <button className="ml-auto mr-2 cursor-pointer" onClick={onRemove} aria-label="remove"
+                role="button"><CloseIcon fontSize='small'/></button>
             </div>) :
-            (<label htmlFor="upload-file" className="flex flex-col items-center justify-center w-full h-32 border-2 border-dashed border-gray-500 rounded-lg cursor-pointer bg-gray-800 hover:border-blue-400 transition duration-300 mt-2">
+            (<label htmlFor="upload-file" className="flex flex-col items-center justify-center w-full h-32 border-2 border-dashed border-gray-500 rounded-lg cursor-pointer bg-[var(--background)] hover:border-blue-400 transition duration-300 mt-2">
               <div>
-                <p className="flex items-center gap-2"><FileUploadOutlinedIcon/> Choose file</p>
-                <p>Upload to see preview</p>
+                <p className="text-text flex items-center gap-2"><FileUploadOutlinedIcon/> Choose file</p>
+                <p className="text-text">Upload to see preview</p>
               </div>
-              <input id="upload-file" type="file" className="hidden" accept="image/png, image/jpg, image/jpeg" onChange={onChange}/>
+              <input id="upload-file" type="file" name="file" aria-label="file" className="hidden" accept="image/png, image/jpg, image/jpeg" onChange={onChange}/>
             </label>)}
             <p className="text-gray-500 text-sm">300 x 300px recommended. JPGs, JPEGs, and PNGs supported.</p>
         </div>
