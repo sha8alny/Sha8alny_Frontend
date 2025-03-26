e30da48985b7b38754667cc68d5c265822a1894f
ZiadHF <ziadhfahmy@yahoo.com>
2025-03-26 05:40:53 +0200

    fixed navbar (tasleema)

--- src/app/components/layout/NavBar.jsx
+++ src/app/components/layout/NavBar.jsx
@@ -1,7 +1,9 @@
 "use client";
 import { useTheme } from "@/app/context/ThemeContext";
 import { usePathname, useRouter } from "next/navigation";
-import NavbarPresentation, { NavBarPresentationSkeleton } from "./NavBarPresentation";
+import NavbarPresentation, {
+  NavBarPresentationSkeleton,
+} from "./NavBarPresentation";
 import { fetchUserProfile } from "@/app/services/userProfile";
 import { useQuery } from "@tanstack/react-query";
 
@@ -9,7 +11,7 @@ import { useQuery } from "@tanstack/react-query";
  * Navbar component that handles the application's navigation bar functionality.
  * This component fetches user profile data, handles theme toggling, and navigation.
  * It displays a skeleton loader during data fetching and proper error states.
- * 
+ *
  * @component
  * @returns {JSX.Element} The rendered NavBar component, showing either the full NavbarPresentation
  * or the NavBarPresentationSkeleton based on the loading/error state of data fetching
@@ -18,11 +20,15 @@ export default function Navbar() {
   const pathName = usePathname();
   const { theme, toggleTheme } = useTheme();
   const router = useRouter();
-  
+
   // TODO: Replace with actual username from auth context
   const username = "ziadhesham";
 
-  const { data: userProfile, isLoading, isError } = useQuery({
+  const {
+    data: userProfile,
+    isLoading,
+    isError,
+  } = useQuery({
     queryKey: ["userProfile", username],
     queryFn: () => fetchUserProfile(username),
   });
@@ -32,21 +38,27 @@ export default function Navbar() {
   };
 
   if (isLoading) {
-    return <NavBarPresentationSkeleton
-    isLoading={true}
-    theme={theme}
-    toggleTheme={toggleTheme}
-    currentPath={pathName}
-    navigateTo={handleNavigation} />;
+    return (
+      <NavBarPresentationSkeleton
+        isLoading={true}
+        theme={theme}
+        toggleTheme={toggleTheme}
+        currentPath={pathName}
+        navigateTo={handleNavigation}
+      />
+    );
   }
 
-  if (isError) {
-    return <NavBarPresentationSkeleton
-    isLoading={false}
-    theme={theme}
-    toggleTheme={toggleTheme}
-    currentPath={pathName}
-    navigateTo={handleNavigation}/>;
+  if (isError || !userProfile) {
+    return (
+      <NavBarPresentationSkeleton
+        isLoading={false}
+        theme={theme}
+        toggleTheme={toggleTheme}
+        currentPath={pathName}
+        navigateTo={handleNavigation}
+      />
+    );
   }
 
   return (
