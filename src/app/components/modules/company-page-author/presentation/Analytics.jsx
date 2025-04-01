1836e4628614fd2ccecfa6dfdcd490836ef0a4da
nadabdallah <nadabdallah25@gmail.com>
2025-04-01 02:53:15 +0200

    Update Analytics

--- src/app/components/modules/company-page-author/presentation/Analytics.jsx
+++ src/app/components/modules/company-page-author/presentation/Analytics.jsx
@@ -1,6 +1,10 @@
+/**
+ * @namespace company-page-author
+ * @module company-page-author
+ */
 function Analytics(){
     return(
-        <div className="text-text w-1/4 border border-[var(--secondary)] rounded-lg p-4 min-h-screen mt-5 mx-5 ">
+        <div className="bg-[var(--foreground)] text-text border shadow-lg rounded-lg p-4 min-h-screen mt-5 mx-5 ">
             <h1 className="text-lg font-bold">Analytics</h1>
         </div>
     );
