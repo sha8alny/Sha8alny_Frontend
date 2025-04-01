a40d3ce1472c64214d5cefa7ede6fb24b97b689e
nadabdallah <nadabdallah25@gmail.com>
2025-04-01 02:50:01 +0200

    Update Edit Container

--- src/app/components/modules/company-page-author/container/EditContainer.jsx
+++ src/app/components/modules/company-page-author/container/EditContainer.jsx
@@ -1,9 +1,11 @@
 "use client"
 import { useState } from "react";
 import { useRef } from "react";
-import SideBarContainer from "./SideBarContainer";
-import Analytics from "../presentation/Analytics";
 import EditPageContainer from "./EditPageContainer";
+/**
+ * @namespace company-page-author
+ * @module company-page-author
+ */
 /**
  * EditContainer manages the layout and functionality for editing a company's profile.
  * It includes a sidebar, an edit form, and analytics. The component also handles logo uploads.
@@ -12,23 +14,10 @@ import EditPageContainer from "./EditPageContainer";
  * @returns {JSX.Element} The rendered EditContainer component.
  */
 
-function EditContainer({username, logo}){
-    const [logoPreview, setLogoPreview] = useState(logo ||null);
-    const logoInputRef = useRef(null);
-    const logoUpload = (e) => {
-        const selectedFile=e.target.files[0];
-        if (selectedFile){
-            setLogoPreview(prev => URL.createObjectURL(selectedFile));
-        }
-    };
-
+function EditContainer({username}){
     return(
-        <div className="flex">
-            <SideBarContainer username={username} logoPreview={logoPreview} logoInputRef={logoInputRef} logoUpload={logoUpload} />
-            <main className="">
-                <EditPageContainer username={username} logoPreview={logoPreview}/>
-            </main>
-            <Analytics/>
+        <div> 
+            <EditPageContainer username={username} />
         </div>
     );
 }
