41e5542f6097103cc2ca5a0a4fc741487c76b623
Salsabil <salsabilmostafa2003@gmail.com>
2025-04-06 08:09:44 -0700

    Add Forget Password functionality with validation and email handling

---
+++ src/app/forget-password/page.js
@@ -0,0 +1,9 @@
+"use client";
+import ForgetPasswordContainer from "../components/modules/signin/container/ForgetPasswordContainer";
+
+
+const ForgetPasswordPage = () => {
+  return <ForgetPasswordContainer />;
+}
+
+export default ForgetPasswordPage;
