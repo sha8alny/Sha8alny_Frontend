0a1e997e25df0c646b0df0df84a19e5dcdca3399
nadabdallah <nadabdallah25@gmail.com>
2025-04-06 11:49:41 +0200

    Added Documentation to CompanyPage Form Container

--- src/app/components/modules/company-page-form/container/CompanyPageFormContainer.jsx
+++ src/app/components/modules/company-page-form/container/CompanyPageFormContainer.jsx
@@ -3,6 +3,11 @@ import { useState } from "react";
 import CreateCompanySectionContainer from "./CreateCompanySectionContainer";
 import CompanyPreviewSectionContainer from "./CompanyPreviewSectionContainer";
 
+/**
+ * @namespace company-page-form
+ * @module company-page-form
+ */
+
 function CompanyPageFormContainer() {
   const [companyName, setCompanyName] = useState("");
   const [companyIndustry, setCompanyIndustry] = useState("");
@@ -12,11 +17,11 @@ function CompanyPageFormContainer() {
   return (
     <div className="flex flex-col lg:flex-row gap-4 p-6 min-h-screen">
       {/*Left Section*/}
-      <section className="w-[55%] p-4 pt-0 rounded-lg ">
+      <section className="flex-1 p-4 pt-0 rounded-lg ">
         <CreateCompanySectionContainer companyName={companyName} setcompanyName={setCompanyName} companyIndustry={companyIndustry} setcompanyIndustry={setCompanyIndustry} companyTagline={companyTagline} setcompanyTagline={setCompanyTagline} file={file} setFile={setFile}/>
       </section>
       {/*Right Section*/}
-      <section className="w-[45%] p-4 rounded-lg h-fit sticky top-4">
+      <section className="flex-1 lg:max-w-[40%] p-4 rounded-lg h-fit sticky top-14">
         <CompanyPreviewSectionContainer companyName={companyName} companyTagline={companyTagline} companyIndustry={companyIndustry} file={file}/>
       </section>
     </div>
