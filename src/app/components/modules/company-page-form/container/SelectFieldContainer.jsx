093e7a58a70912b27cf8f454c6610e5f1ccddf3b
nadabdallah <nadabdallah25@gmail.com>
2025-04-06 11:53:04 +0200

    Replace Value attribute with values in label attribute

--- src/app/components/modules/company-page-form/container/SelectFieldContainer.jsx
+++ src/app/components/modules/company-page-form/container/SelectFieldContainer.jsx
@@ -3,6 +3,10 @@ import { useState } from "react";
 import SelectField from "../presentation/SelectField";
 
 /**
+ * @namespace company-page-form
+ * @module company-page-form
+ */
+/**
  * SelectFieldContainer manages the state for two dropdown fields: Organization Size and Organization Type.
  * It provides the selected values and updates them when the user selects an option.
  *
@@ -15,13 +19,25 @@ function SelectFieldContainer({companySize, setCompanySize, companyType, setComp
     return (
         <div>
             <SelectField label="Organization size" name="company-size" options={[
-            {label:"0-1 employees", value:"1-option"},
-            {label:"2-10 employees", value:"2-option"},{label:"11-50 employees", value:"3-option"},{label:"51-200 employees", value:"4-option"},{label:"201-500 employees", value:"5-option"},{label:"501-1,000 employees", value:"6-option"},{label:"1,001-5,000 employees", value:"7-option"},{label:"5,001-10,000 employees", value:"8-option"},{label:"10,000+ employees", value:"9-option"}]} required optionlabel="size" selectedvalue={companySize} onChange={setCompanySize}/>
+            {label:"0-1 employees", value:"0-1 employees"},
+            {label:"2-10 employees", value:"2-10 employees"},
+            {label:"11-50 employees", value:"11-50 employees"},
+            {label:"51-200 employees", value:"51-200 employees"},
+            {label:"201-500 employees", value:"201-500 employees"},
+            {label:"501-1,000 employees", value:"501-1,000 employees"},
+            {label:"1,001-5,000 employees", value:"1,001-5,000 employees"},
+            {label:"5,001-10,000 employees", value:"5,001-10,000 employees"},
+            {label:"10,000+ employees", value:"10,000+ employees"}]} required optionlabel="size" selectedvalue={companySize} onChange={setCompanySize}/>
             {errors.companySize && <p className="text-red-500 text-sm mt-1">{errors.companySize}</p>}
 
            <SelectField label="Organization type" name="company-type" options={[
-            {label:"Public company", value:"1-option"},{label:"Self-employed", value:"2-option"},{label:"Government agency", value:"3-option"},{label:"Nonprofit", value:"4-option"},
-            {label:"Sole proprietorship", value:"5-option"},{label:"Privately held", value:"6-option"},{label:"Partnership", value:"7-option"}]}
+            {label:"Public company", value:"Public company"},
+            {label:"Self-employed", value:"Self-employed"},
+            {label:"Government agency", value:"Government agency"},
+            {label:"Nonprofit", value:"Nonprofit"},
+            {label:"Sole proprietorship", value:"Sole proprietorship"},
+            {label:"Privately held", value:"Privately held"},
+            {label:"Partnership", value:"Partnership"}]}
             required optionlabel="type" selectedvalue={companyType} onChange={setCompanyType}/>
             {errors.companyType && <p className="text-red-500 text-sm mt-1">{errors.companyType}</p>}
         </div>
