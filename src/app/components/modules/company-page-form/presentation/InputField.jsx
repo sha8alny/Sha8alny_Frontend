3fbc21c6c1f6016e391c490d3da6a47b9c9551c3
nadabdallah <nadabdallah25@gmail.com>
2025-04-06 11:57:48 +0200

    Added Documentation to InputField

--- src/app/components/modules/company-page-form/presentation/InputField.jsx
+++ src/app/components/modules/company-page-form/presentation/InputField.jsx
@@ -1,12 +1,16 @@
+/**
+ * @namespace company-page-form
+ * @module company-page-form
+ */
 import EmergencyRoundedIcon from '@mui/icons-material/EmergencyRounded';
 
-function InputField({label, name , placeholder, required, selectedname,maxLength, onChange}) {
+function InputField({label, name , type, placeholder, required, selectedname,maxLength, onChange}) {
     return (
         <div className="flex flex-col">
             <label htmlFor={name} className="flex text-text items-center mb-2">
                 {label} {required && <EmergencyRoundedIcon style={{fontSize:"10px"}}/>}
             </label>
-            <input className="border text-text px-[8px] py-[6px] rounded-[4px]" type="text" id={name} name={name} placeholder={placeholder} maxLength={maxLength} value={selectedname} onChange={onChange}/>
+            <input className="border text-text px-[8px] py-[6px] rounded-[4px]" type={type} id={name} name={name} placeholder={placeholder} maxLength={maxLength} value={selectedname} onChange={onChange}/>
         </div>
     )
 }
