468442fd3a4a9328783f736b79e9f451badf9e5e
nadabdallah <nadabdallah25@gmail.com>
2025-04-07 20:59:41 +0200

    Make items in sidebar active

--- src/app/components/modules/company-page-author/presentation/SideBar.jsx
+++ src/app/components/modules/company-page-author/presentation/SideBar.jsx
@@ -12,14 +12,14 @@ import EditIcon from '@mui/icons-material/Edit';
 import ImageOutlinedIcon from '@mui/icons-material/ImageOutlined';
 
 
-function SideBar({menuItems=[], pathname, setActive, isModalOpen, setModalOpen, onChangeCover,  onChangeLogo, coverpreview, logo, coverInputRef,  logoInputRef,fileusername, OpenCompanyUserPage}){
+function SideBar({company, menuItems=[], isActive, isModalOpen, setModalOpen, onChangeCover,  onChangeLogo, coverInputRef,  logoInputRef,fileusername, OpenCompanyUserPage}){
     return (
-        <aside className="bg-[var(--foreground)] text-text w-70px flex flex-col mt-5 mx-5 space-y-4 border rounded-lg p-4">
+        <aside className="bg-[var(--foreground)] text-text w-70px flex flex-col mt-5 mx-3 space-y-4 border rounded-lg p-4">
             {/*Logo and cover image for company*/}
             <div className="relative mb-0 max-h-[160px]">
                 <div className="w-[250px] h-[120px] bg-gray-700 rounded-lg flex justify-center items-center overflow-hidden  relative">
-                    {coverpreview ? (
-                        <img src={coverpreview} alt="cover" className="w-full h-full object-cover" />
+                    {company?.cover ? (
+                        <img src={company?.cover} alt="cover" className="w-full h-full object-cover" />
                     ) : (
                         <ImageOutlinedIcon style={{ width: "100%", height: "100px", color: "gray" }} />
                     )}
@@ -32,8 +32,8 @@ function SideBar({menuItems=[], pathname, setActive, isModalOpen, setModalOpen, 
                 </div>
                 <input id="upload-cover" type="file" className="hidden" accept="image/png, image/jpg, image/jpeg" ref={coverInputRef} onChange={onChangeCover} />
                 <div className="absolute left-4 bottom-[50px] w-[80px] h-[80px] bg-gray-700 rounded-lg flex justify-center items-center overflow-hidden relative">
-                    {logo ? (
-                        <img src={logo} alt="logo" className="w-full h-full object-cover rounded-lg" />
+                    {company?.logo ? (
+                        <img src={company?.logo} alt="logo" className="w-full h-full object-cover rounded-lg" />
                     ) : (
                         <ImageOutlinedIcon style={{ fontSize: "70px", color: "gray" }} />
                     )}
@@ -50,7 +50,7 @@ function SideBar({menuItems=[], pathname, setActive, isModalOpen, setModalOpen, 
             <div>
                 <div className="text-[var(--text)] mb-1 border-b border-[var(--secondary)] pb-2 w-full">
                     <p className="text-2xl font-bold w-full">{fileusername ? fileusername.charAt(0).toUpperCase() + fileusername.slice(1): "Company Name"}</p>
-                    <p className="text-xs"> 7,472,293 followers</p>
+                    <p className="text-xs"> {company?.numFollowers}</p>
                 </div>
                 <div className="border-b border-[var(--secondary)] pb-2 flex flex-row justify-between items-center">
                     <button className="flex items-center gap-2 group cursor-pointer w-fit px-2 py-1 rounded-md transition-all relative" onClick={()=>setModalOpen("create")}>
@@ -66,16 +66,22 @@ function SideBar({menuItems=[], pathname, setActive, isModalOpen, setModalOpen, 
             {/*List of items in dashboard*/}
             <nav>
                 {menuItems.map((item)=>(
-                    <Link key={item.name} href={item.href} className={`group flex items-center rounded-lg gap-2 p-2 ${pathname === item.href ? "bg-[var(--secondary)] " : "hover:bg-[var(--secondary)]"}`} onClick={item.action ? item.action :() => setActive(item.name)} >
+                    <Link
+                    key={item.name}
+                    href={item.href}
+                    className={`group flex items-center rounded-lg gap-2 p-2 mb-1 transition-colors duration-200 ${isActive(item.href)? "bg-[var(--secondary)] text-white": "hover:bg-[var(--secondary)]"}`} onClick={item.action ? item.action : undefined}>
                         {item.icon} {item.name}
                     </Link>))
                 }
-                <button className="relative flex justify-end items-center cursor-pointer group">
-                    <LogoutIcon />
-                    <span className="absolute top-full mt-1 right-1/600 -translate-x-1/2 whitespace-nowrap opacity-0 group-hover:opacity-100 text-[var(--text)] text-xs transition-opacity duration-200 ">
-                        Logout
-                    </span>
-                </button>
+                <div>
+                    <button
+                    className="w-full cursor-pointer flex items-center space-x-2 rounded-lg p-2 text-[var(--text)] hover:bg-[var(--secondary)] transition-colors mt-2"
+                    onClick={() => console.log("Logout clicked")}
+                    >
+                    <LogoutIcon className="text-[var(--text)]" />
+                    <span>Logout</span>
+                    </button>
+                </div>
             </nav>
         </aside>
     );
