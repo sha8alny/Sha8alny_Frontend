598ae1cbf56a7a14c274d8f0f33dc0a13e11bfa6
nadabdallah <nadabdallah25@gmail.com>
2025-04-07 20:43:47 +0200

    Added home page of company page user

---
+++ src/app/company-user-admin/[username]/home-page/page.js
@@ -0,0 +1,11 @@
+"use client";
+import { useParams } from "next/navigation";
+import HomeContainer from "@/app/components/modules/company-page-user/container/HomeContainer";
+export default function HomePage(){
+    const { username } = useParams();
+    return(
+        <div>
+            <HomeContainer username={username} />
+        </div>
+    );
+}
