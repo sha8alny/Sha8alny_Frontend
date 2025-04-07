1cad1ddb08da687315330f194a03c3b29c287251
nadabdallah <nadabdallah25@gmail.com>
2025-04-07 20:58:48 +0200

    Update editpage

--- src/app/components/modules/company-page-author/presentation/EditPage.jsx
+++ src/app/components/modules/company-page-author/presentation/EditPage.jsx
@@ -8,11 +8,12 @@ import EditInputFieldContainer from "../container/EditInputFieldContainer";
 import EditSelectFieldContainer from "../container/EditSelectFieldContainer";
 import TagLineContainer from "../../company-page-form/container/TagLineContainer";
 
-function EditPage({companyName, setCompanyName, companyIndustry, setCompanyIndustry, companyTagline, setCompanyTagline, companySize, setCompanySize, companyType, setCompanyType, companyLocation, setCompanyLocation,companyURL, setCompanyURL, companyWebsite, setCompanyWebsite, onSave, onDiscard, loading, errors, setErrors}){
+function EditPage({companyName, setCompanyName, companyIndustry, setCompanyIndustry, companyTagline, setCompanyTagline, companySize, setCompanySize, companyType, setCompanyType, companyLocation, setCompanyLocation,companyURL, setCompanyURL, companyWebsite, setCompanyWebsite, companyDate, setCompanyDate, companyPhone, setCompanyPhone, onSave, onDiscard, loading, errors, setErrors}){
     return(
         <div>
             <div className="p-4 bg-[var(--foreground)] border rounded-lg">
-                <EditInputFieldContainer companyName={companyName} setCompanyName={setCompanyName} companyIndustry={companyIndustry} setCompanyIndustry={setCompanyIndustry} companyLocation={companyLocation} setCompanyLocation={setCompanyLocation} companyURL={companyURL} setCompanyURL={setCompanyURL} companyWebsite={companyWebsite} setCompanyWebsite={setCompanyWebsite} errorss={errors} setErrors={setErrors}/>
+                <EditInputFieldContainer companyName={companyName} setCompanyName={setCompanyName} companyIndustry={companyIndustry} setCompanyIndustry={setCompanyIndustry} companyLocation={companyLocation} setCompanyLocation={setCompanyLocation} companyURL={companyURL} setCompanyURL={setCompanyURL} companyWebsite={companyWebsite} setCompanyWebsite={setCompanyWebsite} 
+                companyDate={companyDate} setCompanyDate={setCompanyDate} companyPhone={companyPhone} setCompanyPhone={setCompanyPhone} errorss={errors} setErrors={setErrors}/>
                 <EditSelectFieldContainer companySize={companySize} setCompanySize={setCompanySize} companyType={companyType} setCompanyType={setCompanyType}/>
                 <TagLineContainer companyTagline={companyTagline} setCompanyTagline={setCompanyTagline}/>
                 <div className="flex flex-row gap-4">
