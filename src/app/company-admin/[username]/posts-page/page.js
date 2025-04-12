c497ba1edf36eb7ae1cba7b8542996791ffdbd86
nadabdallah <nadabdallah25@gmail.com>
2025-04-12 22:56:54 +0200

    Add Suspense import to PostPage for improved loading state handling

--- src/app/company-admin/[username]/posts-page/page.js
+++ src/app/company-admin/[username]/posts-page/page.js
@@ -1,5 +1,6 @@
 "use client";
 import { useParams } from "next/navigation";
+import { Suspense } from "react";
 import { useSearchParams } from "next/navigation";
 import PostPageContainer from "@/app/components/modules/company-page-author/container/PostPageContainer";
 
