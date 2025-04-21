a7b92ab610cd99c4118c59980278ea8b666fc879
Hussein Essam <112550601+Husseinn-Essam@users.noreply.github.com>
2025-04-21 20:55:33 +0200

    Refactor JobSectionAllPresentation to improve error handling and replace icon with WorkIcon. Update props to include error message for better user feedback.

--- src/app/components/modules/search/presentation/JobSectionAllPresentation.jsx
+++ src/app/components/modules/search/presentation/JobSectionAllPresentation.jsx
@@ -1,31 +1,41 @@
 "use client";
 import ResultsCard from "@/app/components/modules/search/presentation/ResultCard";
-import { Briefcase } from "lucide-react";
+import WorkIcon from "@mui/icons-material/Work";
 import { JobCard } from "@/app/components/modules/jobs/presentation/JobsCard";
 import { normalizeJob } from "@/app/utils/normalizeJob";
 /**
  * JobSectionAllPresentation Component
- * 
+ *
  * This component is responsible for displaying a section of job listings. It handles
  * loading states, error states, and renders a list of job cards or a message if no jobs are found.
- * 
+ *
  * @namespace search
  * @module search
- * 
+ *
  * @param {Object} props - The props object.
  * @param {Array} props.jobs - An array of job objects to be displayed. Each job object is normalized before rendering.
  * @param {boolean} props.isLoading - A flag indicating whether the job data is currently loading.
  * @param {boolean} props.isError - A flag indicating whether there was an error loading the job data.
  * @param {Function} props.onViewMore - A callback function triggered when the "View all job results" button is clicked.
- * 
+ *
  * @returns {JSX.Element} The rendered JobSectionAllPresentation component.
  */
 
- const JobSectionAllPresentation = ({ jobs, isLoading, isError, onViewMore,handleJobClick }) => {
+const JobSectionAllPresentation = ({
+  jobs,
+  isLoading,
+  isError,
+  onViewMore,
+  handleJobClick,
+  error,
+}) => {
   if (isError) {
     return (
-      <ResultsCard title="Jobs" icon={<Briefcase className="h-4 w-4" />}>
-        <p className="text-gray-400 text-sm">Error loading job listings.</p>
+      <ResultsCard
+        title="Jobs"
+        icon={<WorkIcon sx={{ fontSize: "1.125rem" }} />}
+      >
+        <p className="text-gray-400 text-sm">{error}</p>
       </ResultsCard>
     );
   }
@@ -33,13 +43,14 @@ import { normalizeJob } from "@/app/utils/normalizeJob";
     <ResultsCard
       title="Jobs"
       isLoading={isLoading}
-      icon={<Briefcase className="h-4 w-4" />}
+      icon={<WorkIcon sx={{ fontSize: "1.125rem" }} />}
       viewMoreText={"View all job results"}
       onViewMore={onViewMore}
+      flag={jobs?.length > 0 ? true : false}
     >
       {jobs?.length > 0 ? (
         jobs.slice(0, 3).map((job, index) => {
-          if (!job || Object.keys(job).length === 0) return null; 
+          if (!job || Object.keys(job).length === 0) return null;
 
           const normalizedJob = normalizeJob(job);
           console.log(normalizedJob);
@@ -47,7 +58,7 @@ import { normalizeJob } from "@/app/utils/normalizeJob";
           return (
             <JobCard
               key={normalizedJob.id || index}
-              job={normalizedJob} 
+              job={normalizedJob}
               onClick={() => handleJobClick(normalizedJob.id)}
             />
           );
