778c9b0b788d624d7251c29052b32aa33b5b234d
Hussein Essam <112550601+Husseinn-Essam@users.noreply.github.com>
2025-04-30 21:29:02 +0300

    feat: only admins can access admin page

--- src/app/components/layout/QuickAccess.jsx
+++ src/app/components/layout/QuickAccess.jsx
@@ -1,29 +1,43 @@
 import { Bookmark, Insights, Settings } from "@mui/icons-material";
-import { ChartNoAxesCombined } from "lucide-react";
+import { useEffect, useState } from "react";
 import Container from "./Container";
-
-const QuickAccessIcons = [
+import AdminPanelSettingsIcon from '@mui/icons-material/AdminPanelSettings';
+const allQuickAccessIcons = [
   {
     icon: Bookmark,
     title: "My Items",
     path: "/my-items",
   },
   {
-    icon: Insights,
-    title: "Analytics",
-    path: "/analytics",
-  },
-  {
     icon: Settings,
     title: "Settings",
     path: "/settings",
   },
+  {
+    icon: AdminPanelSettingsIcon,
+    title: "System Managment",
+    path: "/admin/analytics",
+    adminOnly: true,
+  },
 ];
 
 const QuickAccess = ({ navigateTo }) => {
+  const [isAdmin, setIsAdmin] = useState(false);
+  const [quickAccessIcons, setQuickAccessIcons] = useState([]);
+
+  useEffect(() => {
+    const adminStatus = localStorage.getItem("isAdmin") === "true";
+    setIsAdmin(adminStatus);
+    
+    const filteredIcons = allQuickAccessIcons.filter(
+      icon => !icon.adminOnly || adminStatus
+    );
+    setQuickAccessIcons(filteredIcons);
+  }, []);
+
   return (
     <Container className="p-4 w-full dark:border-[#111] border shadow-md flex flex-col gap-2">
-      {QuickAccessIcons.map((icon, index) => (
+      {quickAccessIcons.map((icon, index) => (
         <div key={index} className="flex gap-2 items-center">
           <icon.icon sx={{fontSize: "1.3rem"}} className={icon.className}/>
           <button
