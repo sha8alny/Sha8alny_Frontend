2b6fa8b9b21daad9f53efdb3a18968675dfa5983
nadabdallah <nadabdallah25@gmail.com>
2025-05-03 15:07:23 +0300

    Allow reporting company

--- src/app/components/modules/home/presentation/LandingPage.jsx
+++ src/app/components/modules/home/presentation/LandingPage.jsx
@@ -1,9 +1,9 @@
 export default function LandingPage({goToSignIn, goToSignUp, handleGoogleLogIn}){
     return(
-        <div className="flex flex-1 flex-col-reverse md:flex-row items-center justify-center p-8">
+        <div className="flex flex-col-reverse md:flex-row items-center justify-between px-6 py-12 md:py-20 gap-10 max-w-7xl mx-auto">
             {/*Left section*/}
-            <div className="w-full md:w-1/2 flex flex-col items-start justify-center space-y-6 max-w-xl">
-                <h1 className="text-5xl text-text font-xl ">Welcome to your professional community</h1>
+            <div className="w-full md:w-1/2 flex flex-col items-center md:items-start justify-center space-y-6 text-center md:text-left">
+                <h1 className="text-3xl sm:text-4xl lg:text-5xl font-xl text-text ">Welcome to your professional community</h1>
                 <div className="text-text flex flex-col space-y-4 w-full max-w-md">
                     <button className="flex items-center justify-center rounded-full w-full border border-text p-3 cursor-pointer font-medium hover:bg-secondary hover:border-secondary transition-all" onClick={handleGoogleLogIn}>
                     <img src="/google-color-svgrepo-com.svg" alt="Google Logo" className="w-6 h-6 inline-block mr-2.5" />Continue with Google</button>
@@ -18,8 +18,8 @@ export default function LandingPage({goToSignIn, goToSignUp, handleGoogleLogIn})
                 </div>
             </div>
             {/*Right section*/}
-            <div className="flex justify-center w-full md:w-1/2">
-                <img src="/Landing.png" alt="Landing Image" className="w-[700px] min-h-68 "/>
+            <div className="flex justify-center w-full md:w-1/2 md:flex hidden">
+                <img src="/Landing.png" alt="Landing Image" className="w-full max-w-[500px] md:max-w-[600px] lg:max-w-[700px] h-auto"/>
             </div>
         </div>
     );
